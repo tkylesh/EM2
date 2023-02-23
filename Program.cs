95d000ca6883dc137175925d4d28c878cecc9b9c
tkylesh <taylorshuff90@gmail.com>
2023-02-23 14:10:00 -0600

    initial commit.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EnthusiasticMoose2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
