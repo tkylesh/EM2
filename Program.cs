af9b0a34c01863c0de5ea6cd55f83a454fe419e1
tkylesh <taylorshuff90@gmail.com>
2023-02-23 14:22:37 -0600

    phase 3 complete

--- Program.cs
+++ Program.cs
@@ -7,7 +7,41 @@ namespace EnthusiasticMoose2
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the Enthusiastic Moose Simulator!");
-            Console.Write("------------------------------------------------");
+            Console.WriteLine("------------------------------------------------");
+            Console.WriteLine();
+            MooseSays();
+
+            void MooseSays()
+{
+    Console.WriteLine(@"
+                                       _.--^^^--,
+                                    .'          `\
+  .-^^^^^^-.                      .'              |
+ /          '.                   /            .-._/
+|             `.                |             |
+ \              \          .-._ |          _   \
+  `^^'-.         \_.-.     \   `          ( \__/
+        |             )     '=.       .,   \
+       /             (         \     /  \  /
+     /`               `\        |   /    `'
+     '..-`\        _.-. `\ _.__/   .=.
+          |  _    / \  '.-`    `-.'  /
+          \_/ |  |   './ _     _  \.'
+               '-'    | /       \ |
+                      |  .-. .-.  |
+                      \ / o| |o \ /
+                       |   / \   |    H I, I'M  E N T H U S I A S T I C !
+                      / `^`   `^` \
+                     /             \
+                    | '._.'         \
+                    |  /             |
+                     \ |             |
+                      ||    _    _   /
+                      /|\  (_\  /_) /
+                      \ \'._  ` '_.'
+                       `^^` `^^^`
+    ");
+}
         }
     }
 }
